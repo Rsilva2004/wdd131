3616ce2aa2c692c0a3611224dff444571134f156
Rsilva2004 <rafaelbasilva1723@gmail.com>
2026-07-07 21:21:21 -0300

    lets do this!

---
+++ week02/scripts/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+  if (input.value !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+      deleteButton.addEventListener('click', function () {
+    li.remove();
+
+    
+  });
+
+    li.append(deleteButton);
+    list.append(li);
+
+    input.value = '';
+    input.focus();
+    	inputElement.value = "";
+  }
+  
+});
+
