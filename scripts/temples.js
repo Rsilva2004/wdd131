7ce66dbd9bb8d1e7775d8129fd6154125c03d0d5
Rsilva2004 <rafaelbasilva1723@gmail.com>
2026-07-09 00:54:02 -0300

    hmmm...

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+alert("JavaScript is working");
+
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("open");
+    navigation.classList.toggle("open");
+});
